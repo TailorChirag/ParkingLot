d6c53283217097d93a7421ec7cd9de1a0db5fa62
Chirag Tailor <vermachirag4254@gmail.com>
2025-05-26 10:33:12 +0530

    new commit

--- src/main/java/example/com/Main.java
+++ src/main/java/example/com/Main.java
@@ -1,7 +1,22 @@
 package example.com;
 
+import example.com.Controller.TicketController;
+import example.com.Repositories.GateRepository;
+import example.com.Repositories.ParkingLotRepository;
+import example.com.Repositories.TicketRepository;
+import example.com.Repositories.VehicleRepository;
+import example.com.Services.TicketService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        TicketRepository ticketRepository =  new TicketRepository();
+        ParkingLotRepository parkingLotRepository = new ParkingLotRepository();
+        GateRepository gateRepository = new GateRepository();
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        TicketService ticketService = new TicketService(gateRepository,
+                vehicleRepository, parkingLotRepository,ticketRepository);
+
+        TicketController ticketController = new TicketController(ticketService);
     }
 }
